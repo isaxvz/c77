5867e9c13c844fecaf6b185b867c4efdc022b2b6
isaxvz <114624486+isaxvz@users.noreply.github.com>
2023-04-19 19:49:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("Meu primeiro projeto no console")
+
+ var firstName = "Isabela"
+ var lastName = "Cohn Mor"
+
+ var fullName = firstName.concat(lastName);
+ console.log(fullName);
+
+
+ var a = 10;
+ var b = 5;
+
+
+ var add = a + b ;
+ console.log(add);
+
+ var sub = a - b ;
+ console.log(sub);
+
+ var multiple = a * b ;
+ console.log(multiple);
+
+ var divide = a / b ;
+ console.log(divide);
